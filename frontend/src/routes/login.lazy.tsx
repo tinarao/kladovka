d0f6b8105387592bcdd74ea0c162d9ce1d09580c
tinarao <golovindanila1613@gmail.com>
2024-12-02 21:42:20 +0300

    changed the font, made html&css /login page

--- frontend/src/routes/login.lazy.tsx
+++ frontend/src/routes/login.lazy.tsx
@@ -7,27 +7,61 @@ import {
   CardHeader,
   CardTitle,
 } from '@/components/ui/card';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
 
 import { createLazyFileRoute } from '@tanstack/react-router';
+import { Eye, EyeClosed } from 'lucide-react';
+import { useState } from 'react';
 
 export const Route = createLazyFileRoute('/login')({
   component: RouteComponent,
 });
 
 function RouteComponent() {
+  const [passwordInputType, setPasswordInputType] = useState<
+    'text' | 'password'
+  >('password');
+
   return (
     <main className="h-screen flex flex-col items-center justify-center">
-      <Card>
+      <Card className="w-96">
         <CardHeader>
           <CardTitle>Авторизация</CardTitle>
-          <CardDescription>
-            Всего два поля отделяют Вас от возможности быстро и просто хранить
-            файлы.
-          </CardDescription>
         </CardHeader>
-        <CardContent></CardContent>
-        <CardFooter>
+        <CardContent>
+          <div>
+            <Label className="font-medium">Электронная почта</Label>
+            <Input placeholder="Электронная почта" />
+          </div>
+          <div>
+            <Label className="font-medium">Пароль</Label>
+            <div className="flex items-center gap-x-1">
+              <Input type={passwordInputType} placeholder="Электронная почта" />
+              <Button
+                onClick={() => {
+                  if (passwordInputType === 'password') {
+                    setPasswordInputType('text');
+                    return;
+                  }
+
+                  setPasswordInputType('password');
+                }}
+                size="icon"
+                variant="ghost"
+              >
+                {passwordInputType === 'password' ? (
+                  <EyeClosed className="size-4" />
+                ) : (
+                  <Eye className="size-4" />
+                )}
+              </Button>
+            </div>
+          </div>
+        </CardContent>
+        <CardFooter className="flex items-center justify-between">
           <Button>Войти</Button>
+          <Button variant="link">Зарегистрироваться</Button>
         </CardFooter>
       </Card>
     </main>
